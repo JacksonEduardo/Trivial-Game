7ca6836ae50abb8edf5b04fc89e45d15df11ccf4
Jackson Zabala <jacksonzabala@libero.it>
2024-10-16 17:31:35 +0200

    add new animations in welcome page with score banner

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -15,6 +15,8 @@ const Welcome = () => {
   const [scores, setScores] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [animation, setAnimation] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const playerName = (event) => {
     setName(event.target.value.toUpperCase());
@@ -28,9 +30,6 @@ const Welcome = () => {
       alert("Please insert your name to continue");
     }
   };
-
-  // const openPopup = () => setPopup(true);
-  // const closePopup = () => setPopup(false);
 
   useEffect(() => {
     const fetchScores = async () => {
@@ -47,22 +46,40 @@ const Welcome = () => {
         setError("Error importing data");
         console.log("Error fetching scores", e);
       } finally {
-        setTimeout(() => {
-          setLoading(false);
-        }, 2000);
+        setLoading(false);
       }
     };
     fetchScores();
   }, []);
 
+  setTimeout(() => {
+    setAnimation(false);
+  }, 2000);
+
   // if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
+
+  // code to open score list
+
+  // this method is used only when have only one btn action
+  function openClose() {
+    setIsOpen((prev) => !prev);
+    console.log("click per aprire");
+  }
+  // function open() {
+  //   setIsOpen(true);
+  //   console.log("click per aprire");
+  // }
+  // function close() {
+  //   setIsOpen(true);
+  //   console.log("click per aprire");
+  // }
 
   return (
     <main className="welcomeContainer">
       <section className="welcomeSection">
         <div className="titleUpDown">
-          {loading ? (
+          {animation ? (
             <div className="circleContainer">
               <div className="circleOne"></div>
               <div className="circleTwo"></div>
@@ -74,35 +91,63 @@ const Welcome = () => {
             <h1>Trivial</h1>
           </div>
           <div className="titleDown">
-            {loading ? <div className="lineFlash"></div> : null}
-            {!loading && <h1 className="hiddenE">e</h1>}
+            {animation ? <div className="lineFlash"></div> : null}
+            {!animation && <h1 className="hiddenE">e</h1>}
             <h1>Gam</h1>
           </div>
         </div>
-        {!loading && (
-          <div className="nameInputContainer">
-            <input
-              className="nameInput"
-              type="text"
-              value={name}
-              onChange={playerName}
-              required
-              maxLength={20}
-            />
-            <button className="btnNameInput" onClick={handleStart}>
-              Start
-            </button>
+        <div className="nameInputContainer">
+          {!animation && (
+            <>
+              <input
+                className="nameInput"
+                type="text"
+                value={name}
+                onChange={playerName}
+                required
+                maxLength={20}
+              />
+              <button className="btnNameInput" onClick={handleStart}>
+                Start
+              </button>
+            </>
+          )}
+        </div>
+      </section>
+      {loading ? (
+        <p>Loadgin</p>
+      ) : (
+        <section
+          className={`scoreList ${isOpen ? "scoreListOpen" : "scoreListClose"}`}
+          onClick={openClose}
+        >
+          <div className="btnTitleTable">
+            <div className="openCloseList"></div>
+            <h2>Best Scores</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Category</th>
+                  <th>Score</th>
+                  <th>Mode</th>
+                </tr>
+              </thead>
+              <tbody>
+                {scores.map((el) => (
+                  <tr key={el.id}>
+                    <td>{el.name}</td>
+                    <td>
+                      <p>{el.category}</p>
+                    </td>
+                    <td>{el.score} / 10</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-        )}
-      </section>
-      <section>
-        <h2>Saved Scores</h2>
-        {scores.map((el) => (
-          <p key={el.id}>
-            {el.name}: {el.category} Score: {el.score} / 10
-          </p>
-        ))}
-      </section>
+        </section>
+      )}
     </main>
   );
 };
