3f7e2ce421de51992bdbebf3a5a1a5de206f0782
Jackson Zabala <jacksonzabala@libero.it>
2024-11-25 21:25:58 +0100

    more animation in card gamePage

--- src/components/popupGame.js
+++ src/components/popupGame.js
@@ -4,7 +4,16 @@ import "../style/popupGame.css";
 import React, { useState, useEffect } from "react";
 
 // to save in Firebase database NoSql
-import { collection, addDoc } from "firebase/firestore";
+// import { collection, addDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  limit,
+  orderBy,
+} from "firebase/firestore";
+
 import { db } from "../firebase";
 import { useGameContext } from "../logic/globalContext";
 
@@ -19,11 +28,12 @@ const PopupGame = ({
   const { setPlayerName } = useGameContext(); // code to change name in Global context
   const [newNeme, setNewName] = useState(""); // state to save new name
   const [confirmSave, setConfirmSave] = useState(false);
+  const [error, setError] = useState(null);
 
   const [positions, setPositions] = useState({
     infoSave: 0,
-    saveMsg: 300,
-    scoreList: 600,
+    saveMsg: 400,
+    scoreList: 800,
   });
 
   // function to control edit name
@@ -59,10 +69,10 @@ const PopupGame = ({
   // Gestione aggiornamento posizioni con `useEffect`
   useEffect(() => {
     if (confirmSave) {
-      setPositions({ infoSave: -300, saveMsg: 0, scoreList: 300 });
+      setPositions({ infoSave: -400, saveMsg: 0, scoreList: 400 });
 
       const timeout = setTimeout(() => {
-        setPositions({ infoSave: -600, saveMsg: -300, scoreList: 0 });
+        setPositions({ infoSave: -800, saveMsg: -400, scoreList: 0 });
       }, 3000);
 
       // Pulisce il timeout in caso di smontaggio del componente
@@ -70,45 +80,81 @@ const PopupGame = ({
     }
   }, [confirmSave]);
 
+  // Codice per prendere i dati dal database
+  useEffect(() => {
+    const fetchScores = async () => {
+      try {
+        const scoresCollection = collection(db, "scores");
+        // catch only ten records
+        const scoresQuery = query(
+          scoresCollection,
+          orderBy("score", "desc"),
+          limit(10)
+        );
+        const scoresSnapshot = await getDocs(scoresQuery);
+        const scoreList = scoresSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // setScores(scoreList);
+        console.log(scoreList);
+      } catch (e) {
+        setError("Error importing data");
+        console.log("Error fetching scores", e);
+      }
+      // finally {
+      //   setLoading(false);
+      // }
+    };
+    fetchScores();
+  }, []);
+
+  if (error) return <p>{error}</p>;
   return (
     <div className="popupGameContainer">
-      {/* SAVE SECTION */}
       <div className="infoSavePopup" style={{ top: `${positions.infoSave}px` }}>
-        <h3>{congratulation}</h3>
-        <h3>{playerName}</h3>
-        {/* <h3>{difficulty}</h3> */}
-        {/* <h3>{category}</h3> */}
-        <h3>{score} out of 10 correct!</h3>
-        <h3>Do you want to save your score?</h3>
-        <button
-          className="btnGeneral"
-          onClick={saveScore}
-          disabled={confirmSave}
-        >
-          Save and Publish
-        </button>
-        <p className="infoBeforeSave">
-          Are you sure you want to publish using this name?
-        </p>
-        <p className="infoBeforeSave">
-          If the name is deemed inappropriate, the administrator may delete your
-          entry.
-        </p>
-        <div className="changeName">
-          <input
-            type="text"
-            maxLength={15}
-            value={newNeme}
-            placeholder="Change name"
-            onChange={handleInputNewName}
-            // value={name}
-          />
-          <button className="btn" onClick={handleChangeName}>
-            Confirm changes
-          </button>
+        <div className="topCard">
+          <div className="scorePopup">
+            <h2>{congratulation}</h2>
+            <h4>{playerName}</h4>
+          </div>
+          <h3>Do you want to save your score?</h3>
+        </div>
+        <div className="centerCard">
+          <div className="leftGame"></div>
+          <div className="centerSaveBtn">
+            <button
+              className="btnGeneral"
+              onClick={saveScore}
+              disabled={confirmSave}
+            >
+              Save and Publish
+            </button>
+          </div>
+          <div className="rightGame"></div>
+        </div>
+        <div className="bottomCard">
+          <div className="changeNameContainer">
+            <p className="infoBeforeSave">
+              Are you sure you want to use this name?
+            </p>
+            <p className="infoBeforeSave">
+              If the name is deemed inappropriate, the administrator may delete
+              your game entry.
+            </p>
+            <input
+              type="text"
+              maxLength={15}
+              value={newNeme}
+              placeholder="Change name"
+              onChange={handleInputNewName}
+            />
+            <button className="btn" onClick={handleChangeName}>
+              Confirm name
+            </button>
+          </div>
         </div>
       </div>
-
       {/* SAVE MESSAGE CORRECT */}
       <div className="saveMsgPopup" style={{ top: `${positions.saveMsg}px` }}>
         <h1>Salvatagio corretto</h1>
@@ -125,6 +171,62 @@ const PopupGame = ({
         <button className="btnGeneral">Replay</button>
       </div>
     </div>
+
+    // <div className="popupGameContainer">
+    //   {/* SAVE SECTION */}
+    //   <div className="infoSavePopup" style={{ top: `${positions.infoSave}px` }}>
+    //     <h3>{congratulation}</h3>
+    //     <h3>{playerName}</h3>
+    //     {/* <h3>{difficulty}</h3> */}
+    //     {/* <h3>{category}</h3> */}
+    //     <h3>{score} out of 10 correct!</h3>
+    //     <h3>Do you want to save your score?</h3>
+    //     <div className="changeName">
+    //       <input
+    //         type="text"
+    //         maxLength={15}
+    //         value={newNeme}
+    //         placeholder="Change name"
+    //         onChange={handleInputNewName}
+    //       />
+    //       <button className="btn" onClick={handleChangeName}>
+    //         Confirm name
+    //       </button>
+    //     </div>
+    //     <div className="containerSave">
+    //       <p className="infoBeforeSave">
+    //         Are you sure you want to use this name?
+    //       </p>
+    //       <p className="infoBeforeSave">
+    //         If the name is deemed inappropriate, the administrator may delete
+    //         your game entry.
+    //       </p>
+    //       <button
+    //         className="btnGeneral"
+    //         onClick={saveScore}
+    //         disabled={confirmSave}
+    //       >
+    //         Save and Publish
+    //       </button>
+    //     </div>
+    //   </div>
+
+    //   {/* SAVE MESSAGE CORRECT */}
+    //   <div className="saveMsgPopup" style={{ top: `${positions.saveMsg}px` }}>
+    //     <h1>Salvatagio corretto</h1>
+    //   </div>
+
+    //   {/* CLASSIFICA DEI PUNTEGGI E NAV */}
+    //   <div
+    //     className="scoreListPopup"
+    //     style={{ top: `${positions.scoreList}px` }}
+    //   >
+    //     <h1>Lista punteggi</h1>
+
+    //     <button className="btnGeneral">Home</button>
+    //     <button className="btnGeneral">Replay</button>
+    //   </div>
+    // </div>
   );
 };
 
